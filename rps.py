eb7d94b68467b8f8d07622239ea4a613c324fd1b
reverse <68628837+rxverse@users.noreply.github.com>
2022-05-07 15:58:19 +0400

    not a bunch of if statements

--- rps.py
+++ rps.py
@@ -1,48 +1,27 @@
 import random
-import time
-rr = "yes"
-while rr == "yes":
-    usr = input("Choose : Rock, Paper, Scissors - ")
-    comp = ["rock", "paper", "scissors"]
-    r = random.choice(comp)
-    print("You chose : "+usr)
-    print("----------------------")
-    print("Computer chose : "+r)
-    print("----------------------")
-    if usr.lower() == "rock":
-        if r == "paper":
-            print("you lose")
-    if usr.lower() == "rock":
-        if r == "rock":
-            print("its a tie")
-    if usr.lower() == "rock":
-        if r == "scissors":
-            print("you win")
-    if usr.lower() == "paper":
-        if r == "paper":
-            print("its a tie")
-    if usr.lower() == "paper":
-        if r == "rock":
-            print("you win")
-    if usr.lower() == "paper":
-        if r == "scissors":
-            print("you lose")
-    if usr.lower() == "scissors":
-        if r == "paper":
-            print("you win")
-    if usr.lower() == "scissors":
-        if r == "rock":
-            print("you lose")
-    if usr.lower() == "scissors":
-        if r == "scissors":
-            print("its a tie")
-    print("----------------------")
-    ans = input("Do you want to play again? Y/N ")
-    if ans.lower() == "y":
-        rr = "yes"
-    elif ans.lower() == "n":
-        rr = "no"
-    else:
-        print("undefined answer, shutting down program")
-        time.sleep(1)
-        rr = "no"
+# var setup before game
+while True:
+    choices = ['rock', 'paper', 'scissors']
+    ans = ['yes', 'y', '']
+# initialization
+    player = ''
+    while player not in choices:
+        player = input('Choose an object: ').lower()
+    computer = random.choice(choices)
+# output and logic
+    print('You chose:', player)
+    print('Computer chose:', computer)
+
+    if player == computer:
+        print('its a tie!')
+
+    if (player == 'rock' and computer == 'scissors') or (player == 'paper' and computer == 'rock') or (player == 'scissors' and computer == 'paper'):
+        print('you win')
+
+    if (player == 'rock' and computer == 'paper') or (player == 'paper' and computer == 'scissors') or (player == 'scissors' and computer == 'rock'):
+        print('you lose')
+    
+    playAgain = input('Do you want to play again (Y/N): ').lower()
+    
+    if playAgain not in ans:
+        break
